ac92c068b1e3ebba6e0db104ecf8310232bbb065
Jale B Abeya <jalebikila.abeya221@student.solbridge.ac.kr>
2024-10-24 00:23:20 +0900

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>I am a third-year AI & Big Data Engineering student...</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Jale Bikila ABEYA</h1>
+      <p>AI & Big Data Engineering Student</p>
+      <p>Woosong University, South Korea</p>
+    </div>
+  );
+};
+
+export default Home;
